858b8094bd7c769b268301c46bd1aade0ea83d37
jean9947 <118857398+jean9947@users.noreply.github.com>
2022-12-06 18:31:07 -0500

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,1 +1,20 @@
 
+// Chihyin's first edit below
+// public class Doctor extends Person{
+
+//     private String specialty;
+
+//     public Doctor(String name, String dateOfBirth, String specialty) {
+//         super(name, dateOfBirth);
+//         this.specialty = specialty;
+//     }
+    
+//     public String getSpecialty() {
+//         return specialty;
+//     }
+
+//     @Override
+//     public String toString() {
+//         return ("Doctor " + super.toString() + ", Specialty: " + getSpecialty());
+//     }
+// }
