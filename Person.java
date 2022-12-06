2fd4f0bf4d041440872eb98fb3e5b31e322c4d6d
jean9947 <118857398+jean9947@users.noreply.github.com>
2022-12-06 18:35:34 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -3,10 +3,14 @@ public abstract class Person {
     
     private final String name;
     private final String dateOfBirth;
+    private final String address;
+    private final String phone;
 
-    public Person(String name, String dateOfBirth) {
+    public Person(String name, String dateOfBirth, String address, String: phone) {
         this.name = name;
         this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.phone = phone;
     }
 
     public String getName() {
@@ -14,6 +18,12 @@ public abstract class Person {
     }
     public String getDateOfBirth() {
         return dateOfBirth;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhone() {
+        return phone;
     }
 
     @Override
