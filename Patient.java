47285877a2d3ec03b498e34e276a46f52165d0ff
jean9947 <118857398+jean9947@users.noreply.github.com>
2022-12-06 18:41:45 -0500

    Update Patient.java
    Chihyin's first simple edit.

--- Patient.java
+++ Patient.java
@@ -1,1 +1,21 @@
 
+public class Patient extends Person {
+
+    private String insuranceCompany;
+    private String insuranceCardNumber;
+
+    public Patient(String name, String dateOfBirth, String address, String phone, String insuranceCompany, String insuranceCardNumber) {
+        super(name, dateOfBirth, address, phone);
+        this.insuranceCompany = insuranceCompany;
+        this.insuranceCardNumber = insuranceCardNumber;
+    }
+
+    public String getInsuranceCompany() {
+        return insuranceCompany;
+    }
+    public String getInsuranceCardNumber() {
+        return insuranceCardNumber;
+    }
+
+
+}
