39f9ca8e2febc741050ac1e7f25405aa6a9376c5
jean9947 <118857398+jean9947@users.noreply.github.com>
2022-12-07 11:43:02 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -5,21 +5,6 @@ public class Menu {
     
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("=============Medical Clinic Main Menu============");
-        System.out.println("1 Appointment");
-        System.out.println("2 Doctor");
-        System.out.println("3 Patient");
-        System.out.println("4 Treatment");
-        System.out.println("5 Payment");
-        System.out.println("6 Exit system");
-        System.out.println("Please choose one of above (1-6): ");
-        System.out.println("==================================================");
-
-        int choice = scan.nextInt();
-
-
-        scan.close();
 
     }
 
